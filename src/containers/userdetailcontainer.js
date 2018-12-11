61e68a7138f4483d087c01925197a196dda0f819
prafful <prraful@gmail.com>
2018-12-11 15:53:47 +0530

    redux example done

--- src/containers/userdetailcontainer.js
+++ src/containers/userdetailcontainer.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 class UserDetailContainer extends React.Component {
-    state = {  }
+   
     render() { 
-        return ( <div>
-            User Details from the container!!!!
-        </div> );
+        if(this.props.activeUser === null){
+            return (<div>Click name to see details....</div>)
+        }
+        else{
+            return ( 
+                    <div>
+                        User Details from the container!!!!
+                        <br></br>
+                        Name: {this.props.activeUser.first} {this.props.activeUser.last} at {this.props.activeUser.age} says
+                        <br></br>
+                        <b>{this.props.activeUser.description}</b>
+                    </div> )
+        }
+    }
+}
+
+function convertStoreToProps(store){
+    return{
+        activeUser:store.clickeduser
     }
 }
  
-export default UserDetailContainer;
+export default connect(convertStoreToProps)(UserDetailContainer)
